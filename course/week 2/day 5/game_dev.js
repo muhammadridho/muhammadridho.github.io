9dac89ffc076bf2473e6d2bea0dc1221b2523afd
muhammadridho <ulunmuhammadridho@gmail.com>
2017-01-30 17:27:22 +0800

    move project folder

--- course/week 2/day 5/game_dev.js
+++
@@ -1,9 +0,0 @@
-function cek(){
-    for(var index = 0;index < 2;index++){
-       var jawaban = document.getElementById("coba").value;
-        if (jawaban === "salah"){
-            alert("selesai");
-            break;
-        }
-    }
-}
